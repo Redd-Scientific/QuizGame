778240268ef4a96364f1c9e322a8444def846738
madhuvanthi guruprasad <madhupramod@gmail.com>
2013-08-08 16:12:52 -0400

    cosmetic corrections

--- Quiz/Controllers/HomeController.cs
+++ Quiz/Controllers/HomeController.cs
@@ -10,8 +10,7 @@ namespace Quiz.Controllers
     {
         public ActionResult Index()
         {
-            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
-
+            ViewBag.Message = "Physiology Casino";
             return View();
         }
 
